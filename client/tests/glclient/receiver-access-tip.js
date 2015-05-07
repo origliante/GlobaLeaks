ed64bd1f4bcf84377f0499d63a8ae36296c1e3d7
origliante <noone@nowhere.org>
2015-05-07 19:06:23 +0200

    Fix receiver-access-tip.js

--- client/tests/glclient/receiver-access-tip.js
+++ client/tests/glclient/receiver-access-tip.js
@@ -1,25 +1,24 @@
 
 
 describe('globaLeaks receiver tip reading', function() {
-  var login_receiver = function() {
-      browser.get('http://127.0.0.1:8082/login');
-      element(by.model('loginUsername')).sendKeys('recv1');
-      element(by.model('loginPassword')).sendKeys('qwe2qwe2');
-      var cp = element(by.tagName('button')).click();
-      browser.sleep(2000);
-      return cp;
-  }
-
   it('should redirect to /tips upon successful authentication', function() {
-    login_receiver().then(function () {
+    browser.get('http://127.0.0.1:8082/login');
+    element(by.model('loginUsername')).sendKeys('recv1');
+    element(by.model('loginPassword')).sendKeys('qwe2qwe2');
+    element(by.tagName('button')).click().then(function () {
+      browser.sleep(2000);
       expect(browser.getLocationAbsUrl()).toBe('http://127.0.0.1:8082/#/receiver/tips');
     });
   });
 
   it('should be able to access a tip', function() {
-    login_receiver().then(function () {
-      element(by.css('.btn-success')).click().then(function() {
+    browser.get('http://127.0.0.1:8082/login');
+    element(by.model('loginUsername')).sendKeys('recv1');
+    element(by.model('loginPassword')).sendKeys('qwe2qwe2');
+    element(by.tagName('button')).click().then(function () {
+     element(by.css('.btn-success')).click().then(function() {
         //TODO: check tip text
+        expect(1).toBe(0);
       });
     });
   });
