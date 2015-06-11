0c395d0158e7f242c25318a375c11a0e7bfdb5e9
origliante <noone@nowhere.org>
2015-06-11 17:50:58 +0200

    Add isBrowserSupported from #137 comments, Add update links from #969 comments

--- client/app/scripts/browserchecks.js
+++ client/app/scripts/browserchecks.js
@@ -1,3 +1,12 @@
+
 var isBrowserCompatible = function() {
   return !(bowser.msie && bowser.version <= 8);
 };
+
+var isBrowserSupported = function() {
+  if (typeof window !== 'undefined' && window.crypto) {
+    return !!(window.crypto.subtle || window.crypto.webkitSubtle);
+  }
+  return false;
+}
+
